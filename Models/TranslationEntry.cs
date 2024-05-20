cd0baad37277529d33065d95030e194b40e708ee
Jan Julius <jdelang@hotmail.com>
2024-05-20 20:35:39 +0200

    Add project files.

---
+++ Models/TranslationEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace wordcollector.Models
+{
+    public class TranslationEntry
+    {
+        public string Japanese { get; set; }
+        public string Kanji { get; set; }
+        public string English { get; set; }
+    }
+}
