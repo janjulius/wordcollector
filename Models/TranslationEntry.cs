4e97f1565cba50164446b28bb4e7c5b07540902b
Jan Julius <jdelang@hotmail.com>
2024-05-20 21:06:17 +0200

    done

--- Models/TranslationEntry.cs
+++ Models/TranslationEntry.cs
@@ -1,9 +1,13 @@
-﻿namespace wordcollector.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wordcollector.Models
 {
     public class TranslationEntry
     {
+        [Required(ErrorMessage = "Japanese is required")]
         public string Japanese { get; set; }
         public string Kanji { get; set; }
+        [Required(ErrorMessage = "English is required")]
         public string English { get; set; }
     }
 }
